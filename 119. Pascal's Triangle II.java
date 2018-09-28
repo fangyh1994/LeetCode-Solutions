44bec52022e01d3bba57e07d35ebe7bc6b365079
fangyh1994 <31147954+fangyh1994@users.noreply.github.com>
2018-09-28 12:12:00 -0400

    solve problem 119

---
+++ 119. Pascal's Triangle II.java
@@ -0,0 +1,34 @@
+/*
+Given a non-negative index k where k ¡Ü 33, return the kth index row of the Pascal's triangle.
+
+Note that the row index starts from 0.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example:
+
+Input: 3
+Output: [1,3,3,1]
+Follow up:
+
+Could you optimize your algorithm to use only O(k) extra space?
+*/
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (rowIndex < 0){
+            return null;
+        }
+        for (int i = 0; i <= rowIndex; i++){
+            for (int j = result.size()-1; j >= 1; j--){
+                int tempj = result.get(j);
+                int tempjlo= result.get(j-1);
+                result.set(j, tempj + tempjlo);
+            }
+            result.add(1);
+        }
+        return result;
+    }
+}
