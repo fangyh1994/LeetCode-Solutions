ca7d67b28ce1b13e98d83219e65a74cc111199bd
fangyh1994 <31147954+fangyh1994@users.noreply.github.com>
2019-07-27 20:47:19 +0800

    solve problem 104, problem 107

---
+++ 107. Binary Tree Level Order Traversal II.java
@@ -0,0 +1,52 @@
+/*
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int qSize = q.size();
+            List<Integer> tempList = new ArrayList<Integer>();
+            for (int i = 0; i < qSize; i++) {
+                TreeNode currNode = q.poll();
+                tempList.add(currNode.val);
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+            ans.add(0, tempList);
+        }     
+        return ans;
+    }
+}
