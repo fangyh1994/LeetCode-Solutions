13390a0e3aa6109bb7e3ba8d53e223185a1b15c6
fangyh1994 <31147954+fangyh1994@users.noreply.github.com>
2019-08-22 21:12:18 +0800

    solve problem 147

---
+++ 147. Insertion Sort List.java
@@ -0,0 +1,37 @@
+/*
+Sort a linked list using insertion sort.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if( head == null ){
+            return head;
+        }
+        
+        ListNode helper = new ListNode(0);
+        ListNode cur = head; 
+        ListNode pre = helper;
+        ListNode next = null;
+        //not the end of input list
+        while(cur != null ){
+            next = cur.next;
+            //find the right place to insert
+            while( pre.next != null && pre.next.val < cur.val ){
+                pre = pre.next;
+            }
+            //insert between pre and pre.next
+            cur.next = pre.next;
+            pre.next = cur;
+            pre = helper;
+            cur = next;
+        }       
+        return helper.next;
+    }
+}
