8a0c125896800b74d3170151fba0fe28a7236de0
fangyh1994 <31147954+fangyh1994@users.noreply.github.com>
2019-09-02 22:43:34 +0800

    solve problem 150

---
+++ 150. Evaluate Reverse Polish Notation.java
@@ -0,0 +1,35 @@
+/*
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Note:
+
+Division between two integers should truncate toward zero.
+The given RPN expression is always valid. That means the expression would always evaluate to a result and there won't be any divide by zero operation.
+Example 1:
+
+Input: ["2", "1", "+", "3", "*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+*/
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (tokens[i].equals("-")) {
+                stack.push(-stack.pop() + stack.pop());
+            } else if (tokens[i].equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (tokens[i].equals("/")) {
+                int value = stack.pop();
+                stack.push(stack.pop() / value);
+            } else {
+                stack.push(Integer.valueOf(tokens[i]));
+            }
+        }
+        return stack.pop();
+    }
+}
